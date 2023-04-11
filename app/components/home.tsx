2fab44221638c925d4035582d0d0aa90ab46cfd4
x-zhang_mstr <x.zhang@microstrategy.com>
2023-04-11 21:39:16 +0800

    loading

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -34,9 +34,13 @@ import { useDebounce } from "use-debounce";
 
 export function Loading(props: { noLogo?: boolean }) {
   return (
-    <div className={styles["loading-content"]}>
-      {!props.noLogo && <BotIcon />}
-      <LoadingIcon />
+    <div className={styles.spinner}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
     </div>
   );
 }
